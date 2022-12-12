8495573be2735f773ebae83be7ef3517762e0617
monezero <monezerolindo@gmail.com>
2022-12-12 18:28:44 -0300

    libs, path mapping, folders and start of components

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Container, Title } from "./styles";
+export function SignIn() {
+  return (
+    <Container>
+      <Title>Bem-vindos</Title>
+    </Container>
+  );
+}
